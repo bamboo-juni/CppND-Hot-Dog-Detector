0423a92ab9414c374202a62e42e12a207e00adc8
junyue <wujy.03@hotmail.com>
2020-07-09 16:52:42 +0200

    add image input & output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <opencv2/highgui.hpp>
 
 int main() {
-    std::cout << "Hello World!" << "\n";
+
+    // read image from file
+    cv::Mat img = cv::imread("../data/hotdog.jpg", -1);
+    if( img.empty() ) return -1;
+
+    // display image
+    cv::namedWindow( "Hot Dog", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "Hot Dog", img );
+    
     return 0;
 }
